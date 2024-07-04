0a778b972a8170e4ec14f53a4c45dff363b82723
Ajay gladson <ajaygladson564@gmail.com>
2024-07-04 11:29:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './Components/Login';
-import Quiz from './Components/Quiz';
-// import Quiz from './Components/Quiz';
+import Quiz from './Components/QuizApp';
+
 
 function App() {
   return (
     
-      <BrowserRouter>
-       <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/home" element={<Quiz/>}/>
-       </Routes>
-      </BrowserRouter>
+      <Quiz/>
     
   );
 }
